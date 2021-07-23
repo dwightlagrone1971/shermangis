4db37dd8c66db5c49b1553141a35821e6d9c72ab
Dwight LaGrone <dwightlagrone@yahoo.com>
2021-07-23 16:03:45 -0500

    Commit-062321-v10

--- src/components/Infographics.vue
+++ src/components/Infographics.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="">
-    <p class=" pt-14 pb-20 text-xl font-bold mr-96 ml-96" style="color: #4a4a4a;">
+    <p class=" pt-14 pb-20 text-xl font-bold mr-5 ml-5 md:mr-48 md:ml-48xl:mr-96 xl:ml-96" style="color: #4a4a4a;">
       <span class="text-3xl">Infographics</span> are a visual representation of information
  or data. The infographics below use American Community
  Survey (ACS), Census, Esri forecasts, and third-party retail data.</p>
-    <div class="grid md:grid-cols-3 md:ml-96 md:mr-96">
+    <div class="grid md:grid-cols-3 md:ml-48 md:mr-48 xl:mr-96 xl:ml-96">
       <div v-for="item in items" :key="item" class="mb-10 mr-12 ml-12 inline-block transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:transform-none" style="background-color: #4a4a4a" >
         <a :href="item.url" :title="item.name" target="_blank">
           <div class="text-lg p-3" style="color: #f8f8f8">
