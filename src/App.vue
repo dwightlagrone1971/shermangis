69d78ddb89c2ac419396f84a99a478f3098df367
Dwight LaGrone <dwightlagrone@yahoo.com>
2021-09-01 08:29:27 -0500

    Commit-090121-v2

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 
       <Header />
       <Menu />
-      <router-view class="border-2 border-gray-200 bg-gradient-to-r from-gray-300 via-gray-300 to-gray-300 h-screen"/>
+      <router-view class="h-screen"/>
       <Footer id="footer"/>
 
   </div>
