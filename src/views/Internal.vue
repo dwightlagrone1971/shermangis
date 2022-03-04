0bec69bfda5731b8b11eb25128d3a2cd3933458c
Dwight LaGrone <dwightlagrone@yahoo.com>
2022-03-04 16:10:15 -0600

    Commit - 030422 v2

--- src/views/Internal.vue
+++ src/views/Internal.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="m-auto lg:w-10/12 2xl:w-6/12">
+  <div class="m-auto lg:w-10/12 2xl:w-10/12">
     <h1 class="m-auto mt-12 mb-12 text-5xl 2xl:mt-24 2xl:mb-20 mb-10 font-bold" style="color: #4a4a4a">Internal Maps</h1>
-    <div class="m-auto grid grid-cols-1 lg:grid-cols-3">
+    <div class="m-auto grid grid-cols-1 lg:grid-cols-4 2xl:w-10/12">
       <div
         v-for="item in items"
         :key="item"
