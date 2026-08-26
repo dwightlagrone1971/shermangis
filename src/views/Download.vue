bfbc75b7293a81b19d24bf87cee0885f360f3b35
Dwight LaGrone <dwightlagrone@yahoo.com>
2026-08-26 09:54:50 -0500

    Commit - Update

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-white border-gray-100 border-2 mt-14">
-    <div class="text-blue-900 -bottom-11font-serif m-auto w-10/12 my-10 lg:w-9/12 2xl:w-6/12">
+    <div class="text-blue-900 font-serif m-auto w-10/12 my-10 lg:w-9/12 2xl:w-6/12">
       <p class="text-blue-900 font-serif m-auto mb-6 2xl:py-10 text-lg font-bold">
         <span class="text-4xl text-blue-900">GIS Data </span>use raw datasets in themed dashboards to download information for your websites, applications or research. Search, filter and zoom in to quickly and easily select only the datasets that interest you. Can't find what you are looking for?   <a href="mailto: dwightl@cityofsherman.com" class="text-xl text-blue-500 font-semibold underline text-red-900"
             target="_blank">   Contact us! </a></p>
